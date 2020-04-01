955fab90c3baee4497e4dd1c1f71c1e963f94694
ianiemeka14 <55368986+ianiemeka14@users.noreply.github.com>
2020-03-31 22:32:10 -0400

    Add files via upload

---
+++ Restart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public static bool hasRestarted = false;
+    public void restartScene()
+    {
+        hasRestarted = true;
+        SceneManager.LoadScene("Level");
+        Time.timeScale = 1f;
+
+    }
+}
---
+++ generate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generate : MonoBehaviour
+{
+
+    public GameObject rocks;
+    int score;
+
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", 2.5f, 1f);
+
+       
+        
+    }
+
+   
+    void CreateObstacle()
+    {
+        Instantiate(rocks);
+       
+    
+    }
+
+    
+    void Update()
+    {
+        
+        
+    }
+}
